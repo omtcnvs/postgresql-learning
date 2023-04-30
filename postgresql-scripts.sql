e81ac51abfc3e25df9166281805d64ed05a8ea2c
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-29 22:55:22 -0300

    Adding more querys

--- postgresql-scripts.sql
+++ postgresql-scripts.sql
@@ -1,3 +1,4 @@
+-- CREATING TABLE ALUNOS
 CREATE TABLE alunos(
 	id SERIAL,
 	nome VARCHAR(255),
@@ -12,7 +13,18 @@ CREATE TABLE alunos(
 	matriculado_em TIMESTAMP
 );
 
+-- RETRIEVING ALL DATA FROM ALUNOS
 SELECT * FROM alunos;
 
+-- INSERTING DATA IN TABLE ALUNOS
 INSERT INTO alunos(nome, cpf, observacao, idade, dinheiro, altura, ativo, data_nascimento, hora_aula, matriculado_em)
 VALUES ('Matheus', '27298219022', 'aaaaaaaaaa', 19, 100.50, 1.82, TRUE, '2003-09-08', '19:20:00', '2020-01-01 12:00:00');
+
+-- RETRIEVING DATA WITH CONDITION USING CLAUSULE WHERE
+SELECT * FROM alunos WHERE id = 1;
+
+-- UPDATING ROW IN TABLE ALUNO
+UPDATE alunos
+SET nome = 'Matheus Canovas'
+WHERE id = 1;
+
